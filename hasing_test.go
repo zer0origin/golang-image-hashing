637952d554aeaca19e9306c2b8376acc3f4f3c2c
Callum <callumery11@outlook.com>
2025-11-23 15:57:09 +0000

    Updated file strucutre, corrected file names.

---
+++ hasing_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	_ "embed"
+	"encoding/base64"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed EncodedImageExample1
+var File1 string
+
+//go:embed EncodedImageExample2
+var File2 string
+
+func TestHashingFile1(t *testing.T) {
+	bytes := make([]byte, base64.StdEncoding.DecodedLen(len(File1)))
+	_, err := base64.StdEncoding.Decode(bytes, []byte(File1))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(fmt.Sprintf("Image Size: %2.f kb", float64(len(bytes)/1000)))
+
+	imageHash := ImageHash{}
+	hash := imageHash.HashPageOfDocument(bytes)
+	fmt.Println(hash)
+
+	fmt.Println("\nConvert:")
+	str := imageHash.ConvertHashToString(hash)
+	fmt.Println(str)
+
+	assert.Equal(t, str, "LmPjeyvClM4")
+}
+
+func TestHashingFile2(t *testing.T) {
+	bytes := make([]byte, base64.StdEncoding.DecodedLen(len(File2)))
+	_, err := base64.StdEncoding.Decode(bytes, []byte(File2))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(fmt.Sprintf("Image Size: %2.f kb", float64(len(bytes)/1000)))
+
+	imageHash := ImageHash{}
+	hash := imageHash.HashPageOfDocument(bytes)
+	fmt.Println(hash)
+
+	fmt.Println("\nConvert:")
+	str := imageHash.ConvertHashToString(hash)
+	fmt.Println(str)
+
+	assert.Equal(t, str, "JWqHUeD4")
+}
